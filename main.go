2c3adce14fde4fb634fe5d678ad4dbf4bf4dbe8c
mnielsen000 <Mary.nielsen.ctl@gmail.com>
2018-10-23 15:11:48 -0700

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - now"}
+		res := &response{Message: "Hello World - Hi Ray"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
